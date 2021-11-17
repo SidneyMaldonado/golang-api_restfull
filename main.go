17f325f9898717a43e26f97acd4a4a43186b6e36
SidneyMaldonado <sidneymaldonado@gmail.com>
2021-11-17 16:26:44 -0400

    firs commit

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
+)
+
+type Payload struct {
+	Stuff Data
+}
+type Data struct {
+	Fruit Fruits
+	Veggies Vegetables
+}
+
+type Fruits map[string]int
+type Vegetables map[string]int
+
+func main(){
+
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request ) {
+
+		response := []byte("")
+		response, _ = getJsonResponse()
+
+		fmt.Fprint(w,  string(response) )
+		return
+
+	})
+	http.ListenAndServe(":3000", r)
+
+}
+
+func getJsonResponse()([]byte, error) {
+	fruits := make(map[string]int)
+	fruits["Apples"] = 25
+	fruits["Oranges"] = 10
+
+	vegetables := make(map[string]int)
+	vegetables["Carrats"] = 10
+	vegetables["Beets"] = 0
+
+	d := Data{fruits, vegetables}
+	p := Payload{d}
+
+	return json.MarshalIndent(p, "", "  ")
+}
