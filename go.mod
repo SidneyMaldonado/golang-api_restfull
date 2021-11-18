060bae036f2fe90157d5ebf328a8943cf7a99a25
SidneyMaldonado <sidneymaldonado@gmail.com>
2021-11-18 00:41:27 -0400

    Conectando com o banco de dados

--- go.mod
+++ go.mod
@@ -2,13 +2,16 @@ module basicApiTest
 
 go 1.17
 
+require github.com/go-chi/chi/v5 v5.0.6
+
 require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.7.4 // indirect
-	github.com/go-chi/chi/v5 v5.0.6 // indirect
+	github.com/go-chi/chi v1.5.4 // indirect
 	github.com/go-playground/locales v0.13.0 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/golang/protobuf v1.3.3 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.10.0 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
-	"net/http"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"net/http"
 )
 
 type Payload struct {
@@ -16,25 +18,41 @@ type Data struct {
 	Fruit Fruits
 	Veggies Vegetables
 }
+type City struct {
+	Id         int
+	Name       string
+	Population int
+}
+
+type Cidade struct {
+	codCidade int
+	nome string
+	codestado int
+}
 
 type Fruits map[string]int
 type Vegetables map[string]int
 
+
 func main(){
+
+//	test()
+	var db, _ = conectarBanco()
+
+	query("select * from cidade", db)
 
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request ) {
-
 		response := []byte("")
 		response, _ = getJsonResponse()
 
-		fmt.Fprint(w,  string(response) )
+		_,_ = fmt.Fprint(w,  string(response) )
 		return
 
 	})
-	http.ListenAndServe(":3000", r)
+	_ = http.ListenAndServe(":3000", r)
 
 }
 
@@ -45,11 +63,80 @@ func getJsonResponse()([]byte, error) {
 	fruits["Oranges"] = 10
 
 	vegetables := make(map[string]int)
-	vegetables["Carrats"] = 10
+	vegetables["Cars"] = 10
 	vegetables["Beets"] = 0
 
 	d := Data{fruits, vegetables}
 	p := Payload{d}
 
 	return json.MarshalIndent(p, "", "  ")
+}
+
+func conectarBanco() (*sql.DB, error){
+
+	var banco, err = sql.Open("mysql","u475983679_aula:Senha@01!@tcp(sql395.main-hosting.eu:3306)/u475983679_aula")
+	if err != nil {
+		log.Printf("Error %s when opening DB\n", err)
+	} else {
+		log.Printf("Banco de dados inicializado com sucesso")
+	}
+	return banco, err
+}
+
+func query( comando string, db *sql.DB) (result *sql.Rows,error error){
+
+	var res, err = db.Query(comando)
+
+	defer res.Close()
+
+	if err != nil{
+		log.Printf("Erro ao recuperar dados")
+		log.Printf(err.Error())
+	} else {
+		log.Printf("Dados recuperado")
+		for res.Next(){
+
+			var cidade Cidade
+			err := res.Scan(&cidade.codCidade, &cidade.nome, &cidade.codestado)
+			if err != nil {
+				log.Printf(err.Error())
+			}
+			fmt.Printf("%v\n", cidade)
+		}
+	}
+	if res == nil {
+		log.Printf("sem dados para processar")
+	}
+
+	return res, err
+
+}
+
+func test(){
+	db, err := sql.Open("mysql", "u475983679_aula:Senha@01!@tcp(sql395.main-hosting.eu:3306)/u475983679_aula")
+	defer db.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := db.Query("SELECT * FROM cidade")
+
+	defer res.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for res.Next() {
+
+		var city City
+		err := res.Scan(&city.Id, &city.Name, &city.Population)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%v\n", city)
+	}
 }
