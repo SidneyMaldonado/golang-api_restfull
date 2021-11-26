ee9562ee564d56ed3acfb09732d17cf480155a3c
SidneyMaldonado <sidneymaldonado@gmail.com>
2021-11-26 13:27:28 -0400

    Rota dinamica

--- main.go
+++ main.go
@@ -9,13 +9,14 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type Payload struct {
 	Stuff Data
 }
 type Data struct {
-	Fruit Fruits
+	Fruit   Fruits
 	Veggies Vegetables
 }
 type City struct {
@@ -26,37 +27,74 @@ type City struct {
 
 type Cidade struct {
 	Codcidade int
-	Nome string
+	Nome      string
 	Codestado int
 }
+
+type Registro struct {
+	coluna string
+	dado string
+}
+
+type Tabela struct {
+	nome string
+	registros []Registro
+}
+// falta implementar tabela e registros
 
 type Fruits map[string]int
 type Vegetables map[string]int
 
-
-func main(){
-
-//	test()
-	var db, _ = conectarBanco()
-	var data []byte
-	data, _ = query("select * from cidade", db)
-	db.Close()
+func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request ) {
-		response := []byte("")
-		response = data
-
-		_,_ = fmt.Fprint(w,  string(response) )
-		return
-
-	})
+	r.Get("/{id}", func(w http.ResponseWriter, r *http.Request) { processGet(w,r)} )
+	log.Printf("Server started...")
 	_ = http.ListenAndServe(":3000", r)
 
 }
 
-func getJsonResponse()([]byte, error) {
+func processGet(w http.ResponseWriter, r *http.Request){
+
+	log.Printf("Iniciou get..." + r.URL.Path)
+	var response = []byte("")
+	var err error
+	var routeparts = strings.Split(r.URL.Path, "/")
+	log.Print("Length:", len(routeparts))
+	table := routeparts[1]
+	log.Printf("Table: " + table)
+
+	if table =="favicon.ico"{
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if len(routeparts) != 2 {
+		w.WriteHeader(http.StatusNotFound)
+	} else {
+		response, err = buscarTabela(table)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+	}
+
+	_, _ = fmt.Fprint(w, string(response))
+	return
+}
+func buscarTabela(table string) ([]byte, error) {
+
+	var cmd = "select * from " + table
+	log.Printf(cmd)
+	var db, _ = conectarBanco()
+	var data []byte
+	var err error
+	log.Printf("tabela:", table)
+	data, err = query(cmd, db)
+	db.Close()
+	return data, err
+}
+func getJsonResponse() ([]byte, error) {
 	fruits := make(map[string]int)
 	fruits["Apples"] = 25
 	fruits["Oranges"] = 10
@@ -71,10 +109,10 @@ func getJsonResponse()([]byte, error) {
 	return json.MarshalIndent(p, "", "  ")
 }
 
-func conectarBanco() (*sql.DB, error){
+func conectarBanco() (*sql.DB, error) {
 
-//	var banco, err = sql.Open("mysql","u475983679_aula:Senha@01!@tcp(sql395.main-hosting.eu:3306)/u475983679_aula")
-	var banco, err = sql.Open("mysql","root:@tcp(localhost:3306)/sistema")
+	//	var banco, err = sql.Open("mysql","u475983679_aula:Senha@01!@tcp(sql395.main-hosting.eu:3306)/u475983679_aula")
+	var banco, err = sql.Open("mysql", "root:@tcp(localhost:3306)/sistema")
 	if err != nil {
 		log.Printf("Error %s when opening DB\n", err)
 	} else {
@@ -83,7 +121,7 @@ func conectarBanco() (*sql.DB, error){
 	return banco, err
 }
 
-func query( comando string, db *sql.DB) (result []byte,error error){
+func query(comando string, db *sql.DB) (result []byte, error error) {
 
 	var res, err = db.Query(comando)
 
@@ -91,14 +129,14 @@ func query( comando string, db *sql.DB) (result []byte,error error){
 	defer res.Close()
 	var cidades []Cidade
 
-	if err != nil{
+	if err != nil {
 		log.Printf("Erro ao recuperar dados")
 		log.Printf(err.Error())
 	} else {
 		log.Printf("Dados recuperado")
 
 		var i = 0
-		for res.Next(){
+		for res.Next() {
 
 			var nova Cidade
 			err := res.Scan(&nova.Codcidade, &nova.Nome, &nova.Codestado)
@@ -120,7 +158,7 @@ func query( comando string, db *sql.DB) (result []byte,error error){
 
 }
 
-func test(){
+func test() {
 	db, err := sql.Open("mysql", "u475983679_aula:Senha@01!@tcp(sql395.main-hosting.eu:3306)/u475983679_aula")
 	defer db.Close()
 
